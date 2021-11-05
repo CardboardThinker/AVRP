477e288761bfe43f52a4430d718049b085acafbc
CardboardThinker <91638086+CardboardThinker@users.noreply.github.com>
2021-11-05 18:56:51 +0300

    Add files via upload

---
+++ лабраб14задания4-6/лабраб14задание5/лабраб14задание5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int A,B;
+	cout << "Даны целые положительные числа A и B. Найти их наибольший общий делитель (НОД), используя алгоритм Евклида" << endl;
+	cout << "Введите A" << endl;
+	cin >> A;
+	cout << "Введите B" << endl;
+	cin >> B;
+	while (A != B) {
+		if (B > A) {
+			B = B - A;
+		}
+		else {
+			A = A - B;
+		}
+	}
+	cout << "НОД = " << A << endl;
+	return 0;
+}
---
+++ лабраб14задания4-6/лабраб14задание6/лабраб14задание6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N, F=0, f1 = 0 ,f2 = 1, K = 2;
+	cout << "Дано целое число N (> 1), являющееся числом Фибоначчи: N = F от K. Найти целое число K — порядковый номер числа Фибоначчи N." << endl;
+	cout << "Введите N" << endl;
+	cin >> N;
+	while (F < N) {
+		F = f1 + f2;
+		f1 = f2;
+		f2 = F;
+		K = K + 1;
+	}
+	cout << "K = " << K << endl;
+	return 0;
+}
